04aec829ff313bb0b7eeefa99236b9959181c666
Xursandbek <xursandbek06082001@mail.com>
2023-08-11 11:28:44 +0500

    Sending URL Requests and Parsing JSON response

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -30,6 +30,8 @@ class HomeViewController: UIViewController {
         
         let header = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = header
+        
+        getTrendingMovies()
     }
     
     private func configureNavBar() {
@@ -49,6 +51,18 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds
+    }
+    
+    private func getTrendingMovies() {
+        
+        APICaller.shared.getTrendingMovies { result in
+            switch result {
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
